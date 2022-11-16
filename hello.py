f5c1553bedf33b156491febb2b56ec1c34ca42a2
kushpedia <haronkuria2015@gmail.com>
2022-11-16 10:47:02 +0300

    added list

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
-name=input("name:")
-print("Hello "+ name)
+ # name=input("name:")
+# print(f"Hello {name}") 
+n = int(input("enter number:"))
+ 
+if n > 0:
+  print("number is positive")
+elif n<0:
+    print("n is less than zero")
+else: 
+    print(" n is zero")
